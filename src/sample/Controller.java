846798e3b194909310095c755a51a5c5f185bd18
mlb <mbrunhoej@outlook.com>
2020-02-24 12:30:25 +0100

    Added Initialize function - including update NIC information

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -9,11 +11,26 @@ public class Controller {
     @FXML
     private ComboBox<String> NIC;
 
-    public void setData() {
+    public void initialize () {
+        setNicData();
+
+
+
+
+    }
+
+    public void setNicData() {
+        NIC.getItems().clear();
         for (int i = 0; i < networkInterface.NIC.size(); i++) {
             NIC.getItems().add( networkInterface.NIC.get(i).getName() );
         }
     }
+
+    public void test () {
+        System.out.println(NIC.getButtonCell());
+    }
+
+
 
 
 

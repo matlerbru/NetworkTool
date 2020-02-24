35e746dc455ffe07d66b0d77681d4bc1ab7cd9b3
mlb <mbrunhoej@outlook.com>
2020-02-24 16:05:12 +0100

    Added updateNIC(index) method

--- src/sample/networkInterface.java
+++ src/sample/networkInterface.java
@@ -188,7 +188,92 @@ public class networkInterface {
     }
 
     public static void updateNIC(int index) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder();
 
+        pb.command("cmd.exe", "/c", "ipconfig /all");
+        int readIndex = 0;
+        NIC nic = new NIC();
+
+        try {
+            Process process = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line = null;
+            String lastLine = null;
+            boolean newNIC = false;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.length() == 0) {
+                    try {
+                        if (lastLine.contains(":") && !lastLine.contains(". .")) {
+                            newNIC = true;
+                            nic.displayName = lastLine.replace(":", "");
+                            nic.displayName = nic.displayName.substring( nic.displayName.indexOf("adapter") + 8);
+                        } else {
+                            newNIC = false;
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+
+                if (newNIC) {
+
+                    if (line.contains("Description . . . . . . . . . . . : ")) {
+                        nic.name = line.replace("Description . . . . . . . . . . . : ", "");
+                        nic.name = nic.name.substring(3, nic.name.length());
+                    }
+
+                    if (line.contains("Physical Address. . . . . . . . . : ")) {
+                        nic.MAC = line.replace("Physical Address. . . . . . . . . : ", "");
+                        nic.MAC = nic.MAC.replace(" ", "");
+                    }
+
+                    if (line.contains("IPv4 Address. . . . . . . . . . . : ")) {
+                        nic.IPaddress = line.replace("IPv4 Address. . . . . . . . . . . : ", "");
+                        nic.IPaddress = nic.IPaddress.replace(" ", "");
+                        nic.IPaddress = nic.IPaddress.replace("(Preferred)", "");
+                    }
+
+                    if (line.contains("Subnet Mask . . . . . . . . . . . : ")) {
+                        nic.subnetMask = line.replace("Subnet Mask . . . . . . . . . . . : ", "");
+                        nic.subnetMask = nic.subnetMask.replace(" ", "");
+                    }
+
+                    if (line.contains("Default Gateway . . . . . . . . . : ")) {
+                        nic.defaultGateway = line.replace("Default Gateway . . . . . . . . . : ", "");
+                        nic.defaultGateway = nic.defaultGateway.replace(" ", "");
+                    }
+
+                    if (line.contains("DHCP Enabled. . . . . . . . . . . : ")) {
+                        nic.dhcp = (line.contains("Yes"));
+                    }
+
+                } else {
+                    try {
+                        if (nic.displayName.length() > 0) {
+                            if (readIndex == index) {
+                                NIC.remove(index);
+                                NIC.add(index, nic);
+                            }
+                            System.out.println("readIndex: " + readIndex);
+                            readIndex++;
+
+                            nic = new NIC(null, null, null, null, false, null, null);
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+
+                lastLine = line;
+            }
+            int exitCode = process.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 

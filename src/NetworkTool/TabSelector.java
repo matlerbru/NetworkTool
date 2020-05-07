63fb09618fea7d7d658de1f3d66c52714d241175
mlb <mbrunhoej@outlook.dk>
2020-05-07 11:20:24 +0200

    Getter for NicTool controller added

--- src/NetworkTool/TabSelector.java
+++ src/NetworkTool/TabSelector.java
@@ -1,23 +1,23 @@
 package NetworkTool;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainSelector implements Initializable {
+public class TabSelector implements Initializable {
 
     @FXML
     private TabPane tabPane;
 
     @FXML
     private NicTool nicToolController;
+
+    public NicTool getNicTool() {
+        return nicToolController;
+    }
 
     @FXML
     private NetworkScanner networkScannerController;

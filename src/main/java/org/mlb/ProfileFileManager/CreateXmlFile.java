05afbc70d0acd959796efe87903b06f941e6d686
mlb <mbrunhoej@outlook.dk>
2020-07-14 21:07:45 +0200

    Clean up and refactor

--- src/main/java/org/mlb/ProfileFileManager/CreateXmlFile.java
+++ src/main/java/org/mlb/ProfileFileManager/CreateXmlFile.java
@@ -5,17 +5,14 @@ import java.io.*;
 class CreateXmlFile<file> {
 
     public static File createXmlFile(String fileName) throws IOException {
-        File file = new File(fileName);
-        if  (file.createNewFile()) {
-            printXmlHeaderToFile(file);
-        } else throw new IOException("File already existing: " + fileName);
-        return file;
+        createFileAndPrintHeader(fileName);
+        return new File(fileName);
     }
 
-    private static void printXmlHeaderToFile(File file) {
+    private static void createFileAndPrintHeader(String file) {
         try {
-            PrintStream fileWriter = new PrintStream(new FileOutputStream(file.getName(), true));
-            fileWriter.println("<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>");
+            PrintStream fileWriter = new PrintStream(new FileOutputStream(file));
+            fileWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
             fileWriter.println();
             fileWriter.flush();
             fileWriter.close();
---
+++ src/test/java/org/mlb/ProfileFileManager/CreateXmlFileTest.java
@@ -0,0 +1,47 @@
+package org.mlb.ProfileFileManager;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.util.Scanner;
+
+import org.junit.*;
+
+public class CreateXmlFileTest {
+
+    private String testFilePath = "src/test/resources/CreateXmlFileTest.xml";
+    private File file;
+    private Scanner fileReader;
+    
+    @Before
+    public void checkNoFile() {
+        file = new File(testFilePath);
+        if (file.exists()) {
+            file.delete();
+            assertFalse("Test file exists and can not be deleted", file.exists());
+        }
+        try {
+            CreateXmlFile.createXmlFile(testFilePath); //fejl her, hvorfor kan jeg ikke lave den med sti?
+            fileReader = new Scanner(file);
+        } catch (IOException e) {
+            assertFalse("IOexception", true);
+        }
+    }
+
+    @Test
+    public void createXmlFile() {
+        assertEquals(true, file.exists());
+
+        assertFalse("Created file contains no header", !fileReader.nextLine().contains("xml version="));
+
+        assertFalse("Unwanted content in created file", !fileReader.nextLine().equals(""));
+
+        assertEquals("Created file has too many lines", false, fileReader.hasNextLine());
+    }
+
+    @After
+    public void removeFile() {
+        fileReader.close();
+        file.delete();        
+    }
+}    

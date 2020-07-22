fe4644cdd2935fe8c45943a92c64b9a3fc020542
mlb <mbrunhoej@outlook.dk>
2020-07-22 11:32:59 +0200

    Unit teste for threads utility class

---
+++ src/test/java/org/mlb/Utility/GetAmountAliveTest.java
@@ -0,0 +1,40 @@
+package org.mlb.Utility;
+
+import static org.junit.Assert.*;
+
+import org.mlb.NetworkTool.*;
+
+import java.util.LinkedList;
+
+import org.junit.*;
+
+public class GetAmountAliveTest {
+
+
+    private LinkedList<Thread> threads = new LinkedList<>();
+
+    private int upperTestRange;
+
+    @Before
+    public void initialize() {
+        upperTestRange = (int) ((Math.random() * 100) + 1.0);
+
+        for(int i = 0; i < upperTestRange; i++){
+            threads.add( new Thread(() -> {
+                Threads.sleep(200);
+            }));
+        }
+    }
+
+    @Test
+    public void getAmountAlive(){
+        assertEquals(0, Threads.getAmountAlive(threads));
+
+        for (Thread thread : threads){
+            thread.start();
+        }
+
+        Threads.sleep(100);
+        assertEquals(upperTestRange, Threads.getAmountAlive(threads));
+    }
+} 
---
+++ src/test/java/org/mlb/Utility/WaitForAllToDieTest.java
@@ -0,0 +1,36 @@
+package org.mlb.Utility;
+
+import static org.junit.Assert.*;
+
+import org.mlb.NetworkTool.*;
+import org.mlb.Utility.*;
+
+import java.util.LinkedList;
+
+import org.junit.*;
+
+public class WaitForAllToDieTest {
+
+    private LinkedList<Thread> threads = new LinkedList<>();
+
+    private int upperTestRange;
+
+    @Before
+    public void createThreads() {
+        upperTestRange = (int) ((Math.random() * 100) + 1.0);
+
+        for(int i = 0; i < upperTestRange; i++){
+            threads.add( new Thread(() -> {
+                Threads.sleep(200);
+            }));
+            threads.get(i).start();
+        }
+    }
+
+    @Test
+    public void WaitForAllToDie(){
+        Threads.waitForAllToDie(threads);
+        assertEquals(0, Threads.getAmountAlive(threads));
+    }
+
+} 

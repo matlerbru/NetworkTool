1c31f326d7a905611ad08a5d1964df050db59fa2
mlb <mbrunhoej@outlook.dk>
2020-07-28 21:11:40 +0200

    Method name refactor

--- src/main/java/org/mlb/Utility/TextFormat.java
+++ src/main/java/org/mlb/Utility/TextFormat.java
@@ -3,64 +3,46 @@ package org.mlb.Utility;
 public class TextFormat {
 
     public static boolean isFormattedAsIp(String string) {
-        try {
-            for (int i = 0; i < 3; i++) {
-                if (string.contains(".")) {
-                    int dot = string.indexOf(".");
-                    String temp = string.substring(0, dot);
-                    int value = Integer.parseInt(temp);
-                    if (value < 0 || value > 255) {
-                        throw new Exception();
-                    }
-                    string = string.substring(dot + 1);
-                } else {
-                    throw new Exception();
-                }
-            }
-            int value = Integer.parseInt(string);
-            if (value < 0 || value > 255) {
-                throw new Exception();
-            }
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        return checkFormat(string, 4, ".", 10);
     }   
 
     public static boolean isFormattedAsMac(String string) {
+        return checkFormat(string, 6, "-", 16) && string.length() == 17;
+    }    
 
+    private static boolean checkFormat(String string, int segments, String segmentSeperator, int base) {
         try {
-            for (int i = 0; i < 5; i++) {
-                if (string.contains("-")) {
-                    int hyphen = string.indexOf("-");
-                    String hexValue = string.substring(0, hyphen);
-                    int value = Integer.parseInt(hexValue, 16);
-                    if (value < 0 || value > 255) {
-                        throw new Exception();
-                    }
-                    if (!isOnlyUpperCase(hexValue)) throw new Exception();
-                    if (hexValue.length() != 2) throw new Exception();
-                    string = string.substring(hyphen + 1);
+            for (int i = 0; i < segments - 1; i++) {
+                if (string.contains(segmentSeperator)) {
+                    int seperatorIndex = string.indexOf(segmentSeperator);
+                    String value = string.substring(0, seperatorIndex);
+                    inRange(Integer.parseInt(value, base));
+                    OnlyUpperCase(string);
+                    string = string.substring(seperatorIndex + 1);
                 } else {
                     throw new Exception();
                 }
+
             }
-            int value = Integer.parseInt(string, 16);
-            if (value < 0 || value > 255) {
-                throw new Exception();
-            }
-            if (!isOnlyUpperCase(string)) throw new Exception();
+            inRange(Integer.parseInt(string, base));
+            OnlyUpperCase(string);
             return true;
         } catch (Exception e) {
             return false;
         }
-    }    
-
-    private static boolean isOnlyUpperCase(String string) {
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) > 90) return false;
-        }
-        return true;
-    }
     
+    }
+
+    private static void inRange(int value) throws Exception {
+        if (value < 0 || value > 255) {
+            throw new Exception();
+        }
+    }
+
+    private static void OnlyUpperCase(String string) throws Exception {
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) > 90) throw new Exception();
+        }
+    }
+
 }
